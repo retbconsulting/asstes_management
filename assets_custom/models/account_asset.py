29edc24000e104d07d9f8e5d9e14c18cadc984b9
Mohammed OUCHANI <ouchani.mohammed1@gmail.com>
2023-05-12 12:57:42 +0100

    [ADD] : Add tracking to some fields

--- assets_custom/models/account_asset.py
+++ assets_custom/models/account_asset.py
@@ -6,7 +6,7 @@ class AccountAsset(models.Model):
     
     inventoriable = fields.Boolean(string="Inventoriable", default=True)
     location_id = fields.Many2one(string="Emplacement", comodel_name="stock.location")
-    new_location_id = fields.Many2one(string="Nouvel emplacement", comodel_name="stock.location")
+    new_location_id = fields.Many2one(string="Nouvel emplacement", comodel_name="stock.location", tracking=True)
     product_id = fields.Many2one(string="Article lié", comodel_name="product.product")
     categ_id = fields.Many2one(related="product_id.categ_id")
     barcode = fields.Char(related="product_id.barcode")
@@ -14,4 +14,4 @@ class AccountAsset(models.Model):
     invoice_name = fields.Char(string="N° de facture")
     year = fields.Char(string="Année")
     asset_categ_id = fields.Many2one(comodel_name='asset.category', string='Nature')
-    present = fields.Boolean(string="Est présent", default=False, readonly=True)
+    present = fields.Boolean(string="Est présent", default=False, readonly=True, tracking=True)
