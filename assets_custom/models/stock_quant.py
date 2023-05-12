f3972753f4937de3a31d050c80e4c6726d613c65
Mohammed OUCHANI <ouchani.mohammed1@gmail.com>
2023-05-12 13:33:15 +0100

    [ADD] : Not apply any change if inventoried quantity equal 0

--- assets_custom/models/stock_quant.py
+++ assets_custom/models/stock_quant.py
@@ -8,7 +8,7 @@ class StockQuant(models.Model):
         ### Look for related Asset
         for record in self:
             related_asset = self.env['account.asset'].search([('product_id', '=', record.product_id.id)], limit=1)
-            if related_asset:
+            if related_asset and not record.inventory_quantity:
                 related_asset.write({'present' : True, 'new_location_id' : record.location_id.id})
-            self.write({'inventory_quantity' : 1})
+                self.write({'inventory_quantity' : 1})
         return super(StockQuant, self).action_apply_inventory()
