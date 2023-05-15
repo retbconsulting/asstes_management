2d6715fa233afcf49611ae5425235466e7d33b9b
Mohammed OUCHANI <ouchani.mohammed1@gmail.com>
2023-05-15 15:34:54 +0100

    Reverse last commit

--- assets_custom/models/stock_quant.py
+++ assets_custom/models/stock_quant.py
@@ -8,7 +8,7 @@ class StockQuant(models.Model):
         ### Look for related Asset
         for record in self:
             related_asset = self.env['account.asset'].search([('product_id', '=', record.product_id.id)], limit=1)
-            if related_asset and not record.inventory_quantity:
+            if related_asset:
                 related_asset.write({'present' : True, 'new_location_id' : record.location_id.id})
                 self.write({'inventory_quantity' : 1})
         return super(StockQuant, self).action_apply_inventory()
